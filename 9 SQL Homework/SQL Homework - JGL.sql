41cb7e9002c21a17d1e645eabb72eda081c4bff4
Zhae <jaeglee94@gmail.com>
2019-04-20 15:41:40 -0400

    progress to homework

---
+++ 9 SQL Homework/SQL Homework - JGL.sql
@@ -0,0 +1,28 @@
+-- Activate Sakila Database
+use sakila;
+
+-- 1a. Display the first and last names of all actors from the table actor.
+select first_name,last_name from actor;
+
+-- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
+select concat(first_name, " ", last_name) as "Actor Name" from actor;
+
+-- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." 
+-- What is one query would you use to obtain this information?
+select actor_id,first_name,last_name from actor where first_name like "Joe";
+
+-- 2b. Find all actors whose last name contain the letters GEN:
+select * from actor where last_name like "%gen%";
+
+-- 2c. Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order:
+select last_name, first_name from actor where last_name like "%li%"
+order by last_name, first_name;
+
+-- 2d. Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China:
+select country_id, country from country
+where country in ("Afghanistan","Bangladesh","China");
+
+-- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, 
+-- so create a column in the table actor named description and use the data type BLOB (Make sure to research the type BLOB, 
+-- as the difference between it and VARCHAR are significant).
+
