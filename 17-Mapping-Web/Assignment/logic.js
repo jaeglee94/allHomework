3867c4bfc7d146adcb7e8789a4a4afd219188d2f
Zhae <jaeglee94@gmail.com>
2019-06-21 22:08:22 -0400

    finished homework

--- 17-Mapping-Web/Assignment/logic.js
+++ 17-Mapping-Web/Assignment/logic.js
@@ -2,19 +2,35 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 markers = [];
+
+function getColor(d) {
+  return d <= 1 ? '#ffffb2' :
+        d <= 2 ? '#fed976' :
+        d <= 3 ? '#feb24c' :
+        d <= 4 ? '#fd8d3c' :
+        d <= 5 ? '#f03b20' :
+                    '#bd0026';
+}
+
+
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   for(i=0;i<data.features.length;i++){
     var intensity = ""
-    if(data.features[i].properties.mag*1<=3){
-      intensity = "green"
-    }else if(data.features[i].properties.mag*1<=6){
-      intensity="yellow"
-    }else if(data.features[i].properties.mag*1<=10){
-      intensity="red"
+    if(data.features[i].properties.mag*1<=1){
+      intensity = "#ffffb2"
+    }else if(data.features[i].properties.mag*1<=2){
+      intensity="#fed976"
+    }else if(data.features[i].properties.mag*1<=3){
+      intensity="#feb24c"
+    }else if(data.features[i].properties.mag*1<=4){
+      intensity="#fd8d3c"
+    }else if(data.features[i].properties.mag*1<=5){
+      intensity="#f03b20"
+    }else{
+      intensity="#bd0026"
     }
-
     markers.push(
       L.circle([data.features[i].geometry.coordinates[1],data.features[i].geometry.coordinates[0]], {
         stroke: false,
@@ -73,4 +89,25 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  // Set up the legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function(myMap) {
+    var div = L.DomUtil.create("div", "info legend");
+    var labels = [];
+    var categories = ['0-1','1-2','2-3','3-4','4-5','5+'];
+
+    for(var i=0;i<categories.length;i++){
+      div.innerHTML +=
+      labels.push(
+        '<i class="circle" style="background:' + getColor(i)+ '"></i> ' +(categories[i]?categories[i]:'+')
+      );
+      console.log(getColor(i))
+    }
+    div.innerHTML=labels.join('<br>');
+    return div;
+  };
+
+// Adding legend to the map
+legend.addTo(myMap);
 }
